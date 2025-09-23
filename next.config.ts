9b9c5f5ab37d43e2e42503b5b6afcc7da1f2069f
Nogueira Criativo <nogueiracriativo@gmail.com>
2025-09-23 23:34:43 +0000

    troca todas as imagens por esse logo https://colegioshangrila.com.br/ass

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'colegioshangrila.com.br',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
